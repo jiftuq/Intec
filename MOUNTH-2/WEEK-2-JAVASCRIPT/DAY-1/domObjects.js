f1e5eaff611a9cfe077baf3b6a5add94181b2075
jiftuq <cizac2000@gmail.com>
2020-03-09 13:14:17 +0100

    midday up

---
+++ MOUNTH-2/WEEK-2-JAVASCRIPT/DAY-1/domObjects.js
@@ -0,0 +1,10 @@
+//Find element by classname
+
+var findElementByClassName = document.getElementsByClassName('test');
+console.log(findElementByClassName);
+
+//by Classnname by tagname by id name
+
+//create element
+var newParagraph = document.createElement('p');
+newParagraph.textContent="Lorem ipsum";
